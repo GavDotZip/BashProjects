b045e835d9e8c0cda7cb49540bce548bd915ad34
Gavin Carberry <98050479+GavDotZip@users.noreply.github.com>
2023-05-02 10:14:39 +0100

    first commit

---
+++ src/pingSite.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#ip address, I chose to ping GitHub
+#IP=140.82.121.3
+IP=www.github.com
+
+#ping the address 5 times, with -c we can choose how many times to ping
+#> output.txt ping -i 3 -c 5 $IP
+ping -c 5 $IP &> output.txt
+
+cat output.txt
+#using > we can redirect the output the the ping to another file
+#for this assignment we are calling said file output.txt
